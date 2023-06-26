17554dc771f7a48bd51fb01e391efb98121d329b
Md. Sakibul Alam <mdskrumi@gmail.com>
2023-06-26 18:46:24 +0600

    tab fix

--- src/pages/Components/TabPage.tsx
+++ src/pages/Components/TabPage.tsx
@@ -10,18 +10,20 @@ const TabPage = () => {
             title="Tab"
             breadcrumbs={[{ title: 'Tab', link: '#', disabled: true }]}
         >
-            <Tab
-                tabItems={[
-                    { label: 'Tab 1', value: '1' },
-                    { label: 'Tab 2', value: '2' },
-                    { label: 'Tab 3', value: '3' },
-                    { label: 'Tab 4', value: '4' },
-                ]}
-                value={selected}
-                handleClick={(val: string) => setSelected(val)}
-            />
+            <div className="card p-10 w-fit">
+                <Tab
+                    tabItems={[
+                        { label: 'Tab 1', value: '1' },
+                        { label: 'Tab 2', value: '2' },
+                        { label: 'Tab 3', value: '3' },
+                        { label: 'Tab 4', value: '4' },
+                    ]}
+                    value={selected}
+                    handleClick={(val: string) => setSelected(val)}
+                />
 
-            <div className="text-center">{`Selected tab: ${selected}`}</div>
+                <div className="text-center">{`Selected tab: ${selected}`}</div>
+            </div>
         </MainLayout>
     );
 };
